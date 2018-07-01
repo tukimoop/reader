14daba1200fc216760b87c538db842c8132ba29c
Ashley <ashleyd223@gmail.com>
2018-07-01 18:08:25 +0100

    Remove bouncer class import.

--- app/Modules/Admin/Http/Controllers/DashboardController.php
+++ app/Modules/Admin/Http/Controllers/DashboardController.php
@@ -6,7 +6,6 @@ use Illuminate\Http\Request;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
-use Silber\Bouncer\Bouncer;
 
 class DashboardController extends Controller
 {
