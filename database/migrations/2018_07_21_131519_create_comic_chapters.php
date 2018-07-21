70d23e2b33634079bf1292cdf4ed3ade25d2f08b
Ashley <ashleyd223@gmail.com>
2018-07-21 13:17:37 +0100

    Update packages. Add comic volumes / chapters tables.

---
+++ database/migrations/2018_07_21_131519_create_comic_chapters.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateComicChapters extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comic_chapters', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->string('title');
+            $table->string('title_native')->nullable();
+            $table->unsignedInteger('comic_id');
+            $table->unsignedInteger('number')->comment('The number of this chapter. Used for ordering.');
+            $table->date('release_date');
+            $table->string('cover')->nullable();
+            $table->boolean('quiet_release')->default(0)->comment('Determines if this release should be hidden from latest.');
+            $table->unsignedInteger('comic_volume_id');
+
+            $table->timestamps();
+            $table->softDeletes();
+
+            // Foreign Keys
+            $table->foreign('comic_id')->references('id')->on('comics');
+            $table->foreign('comic_volume_id')->references('id')->on('comic_volumes');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comic_chapters');
+    }
+}
