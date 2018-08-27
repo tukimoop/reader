72730ce6b78e6658c4d6f064363939f9b5f2f1c2
Ashley <ashleyd223@gmail.com>
2018-08-27 23:58:43 +0100

    Fix issue with image viewing.

--- resources/views/comics/read.blade.php
+++ resources/views/comics/read.blade.php
@@ -6,20 +6,16 @@
 
     <h4>Chapter</h4>
 
-    <div class="row">
-        <div class="col-12">
-            @if ($images->isEmpty())
-                <div class="alert alert-info">
-                    This chapter is in the process of being uploaded.
-                </div>
-            @endif
-
-            @foreach ($images as $image)
-
-                    {{ $image->image_url }}
-
-            @endforeach
+    @if ($images->isEmpty())
+        <div class="alert alert-info">
+            This chapter is in the process of being uploaded.
         </div>
-    </div>
+    @endif
+
+    @foreach ($images as $image)
+
+            <img src="{{ $image->image_url }}" class="img-fluid mx-auto d-block">
+
+    @endforeach
 
 @endsection
