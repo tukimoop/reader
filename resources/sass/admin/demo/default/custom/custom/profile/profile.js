55e86122264449c4576c7b2fe14e666a3929fd4c
Ashley <ashleyd223@gmail.com>
2018-09-23 23:15:38 +0100

    Swap files for dist build.

---
+++ resources/sass/admin/demo/default/custom/custom/profile/profile.js
@@ -0,0 +1,1 @@
+"use strict";var KProfile={init:function(){KLib.initMiniChart($("#k_profile_mini_chart_1"),[6,12,9,18,15,9,11,8],KApp.getColor("danger"),2),KLib.initMiniChart($("#k_profile_mini_chart_2"),[8,13,10,14,12,10,11,14],KApp.getColor("brand"),2)}};jQuery(document).ready(function(){KProfile.init()});
